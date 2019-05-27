f7f5ee294da8f8c02b689ee097335237ede36395
Iván Zaera Avellón <izaera@gmail.com>
2019-05-27 16:10:36 +0200

    Create redux-store project
    Run yo liferay-script to create a Shared Bundle project to export a
    Redux store singleton to other projects in this example.
    
    We will use the Redux store to communicate between different portlets
    contained in this example.

---
+++ packages/redux-store/src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Place any initialization of this shared bundle in this function. That way,
+ * you can run:
+ *
+ *              require('redux-store');
+ *
+ * in your dependent projects, before using this shared bundle and you will make
+ * sure that this code is executed once.
+ */
+export default function init() {}
