a4221bb635123279be61156dbb540744bca41aec
Iván Zaera Avellón <izaera@gmail.com>
2019-05-27 17:27:38 +0200

    Load initial data from configuration
    Now that we have a Redux field to store input data, we will get the
    initial value from the portlet instance configuration and dump it into
    the Redux store.
    
    Note that because the configuration field is a simple input text and we
    need multiline data, we replace '|'  by CRs as a way to let users
    specify where CRs occur.

--- packages/input-data-widget/src/AppComponent.js
+++ packages/input-data-widget/src/AppComponent.js
@@ -2,8 +2,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from 'redux-store';
+import actions from 'redux-store/actions';
 
-const AppComponent = () => {
+const AppComponent = ({configuration}) => {
+	const initialCsv = configuration.portletInstance.initialCsv.replace(
+		/\|/g,
+		'\n'
+	);
+
+	store.dispatch(actions.setFormData(initialCsv));
+
 	return (
 		<div>
 			<Provider store={store}>
