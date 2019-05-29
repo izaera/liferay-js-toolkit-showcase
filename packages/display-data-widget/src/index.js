f20d92c05ee336907a7f0b92a6322ef5bc5706e7
Iván Zaera Avellón <izaera@gmail.com>
2019-05-29 10:58:25 +0200

    Create display-data-widget project
    Run yo liferay-script to create a React Widget project.
    
    This project will contain a widget that renders an UI to display data
    that is provided by another widget.
    
    The data will be in CSV format and will consist of body measures
    (weight, fat, muscle, ...) and it will be transferred from the input
    widget to this widget by means of the Redux store.

---
+++ packages/display-data-widget/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * This is the main entry point of the portlet.
+ *
+ * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent
+ * information on the signature of this function.
+ *
+ * @param  {Object} params a hash with values of interest to the portlet
+ * @return {void}
+ */
+export default function main({
+	portletNamespace,
+	contextPath,
+	portletElementId,
+	configuration,
+}) {}
