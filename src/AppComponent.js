e2f41b6b0801b17463aa48edaadd269845680ac2
Iván Zaera Avellón <izaera@gmx.es>
2019-09-02 12:48:45 +0200

    [CP] Add a Material-UI demo
    Demonstrate how to use Material-UI icons and styles engine.
    
    Note that we need to disable the `replace-browser-modules` plugin for
    the `jss-*` packages because their `browser` field points to an invalid
    .js file (in the sense that it tries to use the keyword `export` instead
    of `module.exports` as expected by the bundler).
    
    Also note that we import icons explicitly by file path. There's an
    alternative syntax in Material-UI to import icons directly from
    `@material-ui/icons` but because we use an AMD loader, it causes one
    module definition per icon which is quite poor in terms of performance.

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+
+import StyledMaterialComponent from "./StyledMaterialComponent";
 
 export default class extends React.Component {
-	render() {
-		return (
-            <div>
-				<div>
-        	        <span className="tag">Portlet Namespace:</span> 
-					<span className="value">{this.props.portletNamespace}</span>
-				</div>
-				<div>
-    	            <span className="tag">Context Path:</span>
-					<span className="value">{this.props.contextPath}</span>
-				</div>
-				<div>
-	                <span className="tag">Portlet Element Id:</span>
-					<span className="value">{this.props.portletElementId}</span>
-				</div>
-				
-			</div>
-		);
-	}	
+  render() {
+    return (
+      <div>
+        <p>
+          <div>A styled Material-UI component:</div>
+          <StyledMaterialComponent />
+        </p>
+        <p>
+          <div>An icon from Material-UI:</div>
+          {<AccessAlarmIcon />}
+        </p>
+        <div>
+          <span className="tag">Portlet Namespace:</span>
+          <span className="value">{this.props.portletNamespace}</span>
+        </div>
+        <div>
+          <span className="tag">Context Path:</span>
+          <span className="value">{this.props.contextPath}</span>
+        </div>
+        <div>
+          <span className="tag">Portlet Element Id:</span>
+          <span className="value">{this.props.portletElementId}</span>
+        </div>
+      </div>
+    );
+  }
 }
