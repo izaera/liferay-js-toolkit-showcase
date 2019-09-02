3cf32d3333400c5b4fe9ac9366a920f9b09f6ab1
Iván Zaera Avellón <izaera@gmx.es>
2019-09-02 12:31:14 +0200

    [CP] Add KendoReact button
    Demonstrate KendoReact framework by placing a button styles with
    Material theme.

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "@progress/kendo-theme-material/dist/all.css";
+import { Button } from "@progress/kendo-react-buttons";
 
 export default class extends React.Component {
-	render() {
-		return (
-            <div>
-				<div>
-        	        <span className="tag">Portlet Namespace:</span> 
-					<span className="value">{this.props.portletNamespace}</span>
-				</div>
-				<div>
-    	            <span className="tag">Context Path:</span>
-					<span className="value">{this.props.contextPath}</span>
-				</div>
-				<div>
-	                <span className="tag">Portlet Element Id:</span>
-					<span className="value">{this.props.portletElementId}</span>
-				</div>
-				
-			</div>
-		);
-	}	
+  render() {
+    return (
+      <div>
+        <p>
+          <div>A Button from KendoReact:</div>
+          <Button primary="true">Click me!</Button>
+        </p>
+        <div>
+          <span className="tag">Portlet Namespace:</span>
+          <span className="value">{this.props.portletNamespace}</span>
+        </div>
+        <div>
+          <span className="tag">Context Path:</span>
+          <span className="value">{this.props.contextPath}</span>
+        </div>
+        <div>
+          <span className="tag">Portlet Element Id:</span>
+          <span className="value">{this.props.portletElementId}</span>
+        </div>
+      </div>
+    );
+  }
 }
