1afeb76a89f939d05ef8724fb8a1d3800c05e767
Iván Zaera Avellón <izaera@gmx.es>
2019-11-05 15:00:42 +0100

    [CP] Import a .json file
    Use json-loader to be able to import a .json file and obtain its
    contents as a JS object.

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Button from "./Button";
+import json from "./file.json";
 
 export default class extends React.Component {
   render() {
     return (
       <div>
+        <div>
+          <div>A JSON file:</div>
+          <pre>{JSON.stringify(json, null, 2)}</pre>
+        </div>
         <div>
           <div> A Button from the project:</div>
           <Button />
